3c346f9ec664928c4b3e8e6754d76e5bc387e5d5
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-05-11 12:33:26 +0530

    Add files via upload

--- src/pages/Login/Dashboard/Profile/Profile.js
+++ src/pages/Login/Dashboard/Profile/Profile.js
@@ -1,26 +1,60 @@
-import { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { useState, useRef, useEffect } from "react";
 import classes from "./profile.module.css";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Profile = () => {
-  const [name, setName] = useState("");
+  const [displayName, setDisplayName] = useState("");
   const [photoUrl, setPhotoUrl] = useState("");
+  const nameRef = useRef();
+  const photoRef = useRef();
+
+  
+  
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      const auth = getAuth();
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          const idToken = await user.getIdToken();
+          const response = await fetch(
+            `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCp3CtTZKkTpTX35hg6q4KXdL6fJXTDCgk`,
+            {
+              method: "POST",
+              body: JSON.stringify({ idToken }),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          if (response.ok) {
+            const responseData = await response.json();
+            setDisplayName(responseData.displayName);
+            setPhotoUrl(responseData.photoUrl);
+          } else {
+            console.log("Error fetching profile data:", response.status);
+          }
+        }
+      });
+    };
+
+    fetchProfileData();
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const auth = getAuth();
-    const user = auth.currentUser;
-    const idToken = await user.getIdToken();
+    const enteredName = nameRef.current.value;
+    const updatedPhoto = photoRef.current.value;
 
     const requestBody = {
-      displayName: name,
-      photoUrl: photoUrl,
-      idToken: idToken,
+      displayName: enteredName,
+      photoUrl: updatedPhoto,
       deleteAttribute: [],
       returnSecureToken: true,
     };
 
     try {
+       
       const response = await fetch(
         `https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCp3CtTZKkTpTX35hg6q4KXdL6fJXTDCgk`,
         {
@@ -32,36 +66,40 @@ const Profile = () => {
         }
       );
       if (response.ok) {
+        const responseData = await response.json();
+        setDisplayName(responseData.displayName);
+        setPhotoUrl(responseData.photoUrl);
         console.log("Profile updated successfully");
       } else {
-        console.log("Error updating profile:", response.status.Text);
+        console.log("Error updating profile:");
       }
     } catch (error) {
       console.log(error);
     }
+
+  
   };
 
+
+
   return (
-    <form onSubmit={handleSubmit} className={classes["form-container"]}>
-      <label htmlFor="name">Name:</label>
-      <input
-        type="text"
-        id="name"
-        value={name}
-        onChange={(event) => setName(event.target.value)}
-      />
-      <br />
-      <label htmlFor="photoUrl">Profile Photo URL:</label>
-      <input
-        type="text"
-        id="photoUrl"
-        value={photoUrl}
-        onChange={(event) => setPhotoUrl(event.target.value)}
-      />
-      <br />
-      <button type="submit">Update</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit} className={classes["form-container"]}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" ref={nameRef} defaultValue={displayName} />
+        <br />
+        <label htmlFor="photoUrl">Profile Photo URL:</label>
+        <input type="text" id="photoUrl" ref={photoRef} defaultValue={photoUrl} />
+        <br />
+        <button type="submit">Update</button>
+      </form>
+      <div>
+        <h2>{displayName}</h2>
+        <img src={photoUrl} alt="Profile" />
+      </div>
+    </div>
   );
 };
 
 export default Profile;
+
