db7de4472a5624fefdf841612da645b9d3bd3130
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-05-13 11:51:19 +0530

    Add files via upload

--- src/pages/Login/ExpenseTracker/Tracker.js
+++ src/pages/Login/ExpenseTracker/Tracker.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import classes from "./expensetracker.module.css";
-
+ 
 
 
 const ExpenseTracker = () =>{
@@ -8,6 +8,8 @@ const ExpenseTracker = () =>{
   const [amount, setAmount] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("food");
+  const [editExpense, setEditExpense] = useState(null);
+
 
   useEffect(()=>{
     const fetchData = async () =>{
@@ -35,11 +37,45 @@ const ExpenseTracker = () =>{
   setCategory("Food");  
   };
  
+  const deleteHandler = ()=>{
    
+    localStorage.removeItem("expense");
+    setExpense([]);
+
+  }
+   
+const editHandler = (currentExpense)=>{
+  
+  setEditExpense(currentExpense);
+  setAmount(currentExpense.amount);
+  setDescription(currentExpense.description);
+  setCategory(currentExpense.category);
+};
+
+const submitHandler = (event,expense) =>{
+  event.preventDefault();
+  const updatedExpense = expense.map((item)=>{
+    if(item.timestamp === editExpense.timestamp){
+      return{
+        ...item,
+        amount:+amount,
+        description,
+        category,
+      };
+    }
+    return item;
+  });
+  localStorage.setItem("expense", JSON.stringify(updatedExpense));
+  setExpense(updatedExpense);
+  setAmount("");
+  setDescription("");
+  setCategory("Food");
+  setEditExpense(null);
+};
 
   return (
     <section>
-        <form className={classes.form} onSubmit={addExpenseHandler}>
+        <form className={classes.form} onSubmit={(event) => editExpense ? submitHandler(event, expense) : addExpenseHandler(event)}>
             <label htmlFor="money">Money Spent: $ </label>
             <input type="number" required value={amount} onChange={(event)=>setAmount(event.target.value)}/>
             <label htmlFor="description">Description </label>
@@ -54,14 +90,21 @@ const ExpenseTracker = () =>{
             <button>Add Expense</button>
         </form>
         <div key= {expense.id} className={classes.expense}>
-        {expense.map((expense) => (
-          <div key={expense.timestamp}>
-            <p>Amount: ${expense.amount}</p>
-            <p>Description: {expense.description}</p>
-            <p>Category: {expense.category}</p>
-          </div>
-        ))}
+        {expense.length > 0 && (
+        <div key={expense.id} className={classes.expense}>
+          {expense.map((expense) => (
+            <div key={expense.timestamp}>
+              <p>Amount: ${expense.amount}</p>
+              <p>Description: {expense.description}</p>
+              <p>Category: {expense.category}</p>
+              <button onClick={() => editHandler(expense)} style={{marginBottom:".5rem"}}>Edit</button>
+            </div>
+          ))}
+          <button onClick={deleteHandler}>Delete</button>
+           
         </div>
+      )}
+      </div>
     </section>
   );
 };
