966ea6191396c71ed6884cb9f99a47d070896035
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-05-17 15:34:33 +0530

    Add files via upload

--- src/pages/Login/Dashboard/Dasboard.js
+++ src/pages/Login/Dashboard/Dasboard.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Profile from "./Profile/Profile";
+import { useSelector, useDispatch } from "react-redux";
+import { setExpense } from "../../store/expenseSlice";
+import ExpenseTracker from "../ExpenseTracker/Tracker";
+import axios from "axios";
 
 const Dashboard = () => {
   const [showForm, setShowForm] = useState(false);
   const [emailVerified, setEmailVerified] = useState(false);
+  const [isPremium, setIsPremium] = useState(false);
+
+
+  const dispatch = useDispatch();
+  const expenseList = useSelector((state) => state.expenses);
 
   const idToken = localStorage.getItem("idToken");
-  console.log(idToken);
 
   const checkEmailVerificationStatus = async () => {
     try {
@@ -65,15 +74,47 @@ const Dashboard = () => {
     }
   };
 
-  checkEmailVerificationStatus();
-
-  const logoutHandler = () =>{
+  const logoutHandler = () => {
     localStorage.removeItem("idToken");
-  }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://react-http-d874d-default-rtdb.firebaseio.com/expense.json"
+        );
+        if (response.data) {
+          const loadedExpense = Object.keys(response.data).map((key) => ({
+            id: key,
+            ...response.data[key],
+          }));
+          dispatch(setExpense(loadedExpense));
+          const totalExpense = loadedExpense.reduce(
+            (total, expense) => total + expense.amount,
+            0
+          );
+          if (totalExpense > 10000) {
+            setIsPremium(true);
+          } else {
+            setIsPremium(false);
+          }
+        }
+      
+        }catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [dispatch]);
+
+  useEffect(() => {
+    checkEmailVerificationStatus();
+  }, []);
 
   return (
     <>
-      <Navbar style={{ marginTop: "1rem", backgroundColor:"teal" }}>
+      <Navbar style={{ marginTop: "1rem", backgroundColor: "teal" }}>
         <b>Welcome To Expense Tracker</b>
         {!showForm && (
           <p style={{ marginLeft: "55rem", marginTop: "0rem" }}>
@@ -81,7 +122,11 @@ const Dashboard = () => {
             <button onClick={profileCompleteHandler}>Complete Now</button>
           </p>
         )}
-        <Link to="/login"><button onClick={logoutHandler} style={{ marginLeft: "70rem" }}>Logout</button></Link>
+        <Link to="/login">
+          <button onClick={logoutHandler} style={{ marginLeft: "70rem" }}>
+            Logout
+          </button>
+        </Link>
       </Navbar>
       <hr />
       {!emailVerified && (
@@ -90,10 +135,14 @@ const Dashboard = () => {
           <button onClick={sendVerificationEmail}>Verify Your Email</button>
         </p>
       )}
-
+      <ExpenseTracker expenseList={expenseList} />
+      {isPremium && (
+        <p style={{ marginLeft: "2rem", marginTop: "0rem" }}>
+          Expenses exceed 10,000 rupees! Activate Premium Now{" "}
+          <button>Activate Premium</button>
+        </p>
+      )}
       {showForm && <Profile />}
-      
-    
     </>
   );
 };
